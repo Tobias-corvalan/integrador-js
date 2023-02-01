631c589a38f1dc92661f0bf69d3dc7f058d53518
Tobias Corvalan <tobicorvalan@gmail.com>
2023-02-01 01:58:37 -0300

    integrador js

---
+++ JS/main.js
@@ -0,0 +1,499 @@
+const lista = [
+    {
+        id: 1,
+        nombre: "spiderman",
+        descripcion: "",
+        img: "./img/Spiderman-zombie-hunter.jpg",
+        precio: 100
+    },
+    {
+        id: 2,
+        nombre: "spiderman",
+        descripcion: "",
+        img: "../img/Spiderman/Spiderman-Miles-Morales.jpg",
+        precio: 100
+    },
+    {
+        id: 3,
+        nombre: "spiderman",
+        descripcion:"",
+        img: "../img/Spiderman/Spiderman-antiOck.jpg ",
+        precio: 100
+    },
+    {
+        id: 4,
+        nombre: "spiderman",
+        descripcion: "",
+        img: "../img/Spiderman/Spiderman-Cyborg.jpg" ,
+        precio: 100
+    },
+    {
+        id: 5,
+        nombre: "spiderman",
+        descripcion: "",
+        img: "../img/Spiderman/Spiderman-no-way-home.jpg",
+        precio: 100
+    },
+    {
+        id: 6,
+        nombre: "spiderman",
+        descripcion: "",
+        img: "../img/Spiderman/Spiderman-ps4.jpg",
+        precio: 100
+    },
+    {
+        id: 7,
+        nombre: "spiderman",
+        descripcion: "",
+        img: "../img/Spiderman/Spiderman-Tobey-Maguire.jpg",
+        precio: 100
+    },
+    {
+        id: 8,
+        nombre: "spiderman",
+        descripcion: "",
+        img: "../img/Spiderman/Spiderman-traje-casero.jpg",
+        precio: 100
+    },
+    {
+        id: 9,
+        nombre: "spiderman",
+        descripcion: "",
+        img: "../img/Spiderman/Spiderman-traje-sigilo.jpg",
+        precio: 100
+    },
+    {
+        id: 10,
+        nombre: "spiderman",
+        descripcion: "",
+        img: "../img/Spiderman/Spiderman-vs-Buitre.jpg",
+        precio: 100
+    },{
+        id: 11,
+        nombre: "capitan america",
+        descripcion: "",
+        img: "../img/Capitan America/Capitan-America-2.jpg",
+        precio: 100
+    },
+    {
+        id: 12,
+        nombre: "capitan america",
+        descripcion: "",
+        img: "../img/Capitan America/Capitan-America-2012.jpg",
+        precio: 100
+    },
+    {
+        id: 13,
+        nombre: "capitan america",
+        descripcion: "",
+        img: "../img/Capitan America/Capitan-America-End-Game.jpg",
+        precio: 100
+    },
+    {
+        id: 14,
+        nombre: "capitan america",
+        descripcion: "",
+        img: "../img/Capitan America/Capitan-America-Sam-Wilson.jpg",
+        precio: 100
+    },
+    {
+        id: 15,
+        nombre: "iron man",
+        descripcion: "",
+        img: "../img/Iron Man/Iron Man Mark XLVI.jpg",
+        precio: 100
+    },
+    {
+        id: 16,
+        nombre: "iron man",
+        descripcion: "",
+        img: "../img/Iron Man/Iron-Man-Mark-L.jpg",
+        precio: 100
+    },
+    {
+        id: 17,
+        nombre: "iron man",
+        descripcion: "",
+        img: "../img/Iron Man/Iron-Man-Mark-LXXXV-Tony-Stark.jpg",
+        precio: 100
+    },
+    {
+        id: 18,
+        nombre: "iron man",
+        descripcion: "",
+        img: "../img/Iron Man/iron-man-mark-lxxxv.jpg",
+        precio: 100
+    },
+    {
+        id: 19,
+        nombre: "iron man",
+        descripcion: "",
+        img: "../img/Iron Man/Iron-Man-Mark-XLII.jpg",
+        precio: 100
+    },
+    {
+        id: 20,
+        nombre: "iron man",
+        descripcion: "",
+        img: "../img/Iron Man/Mark-1.jpg",
+        precio: 100
+    },
+    {
+        id: 21,
+        nombre: "iron man",
+        descripcion: "",
+        img: "../img/Iron Man/Reactor-arc.jpg",
+        precio: 100
+    },
+    {
+        id: 22,
+        nombre: "iron man",
+        descripcion: "",
+        img: "../img/Iron Man/Tony-Stark.jpg",
+        precio: 100
+    },
+    {
+        id: 23,
+        nombre: "thor",
+        descripcion: "",
+        img: "../img/Thor/Loki-2012.jpg",
+        precio: 100
+    },
+    {
+        id: 24,
+        nombre: "thor",
+        descripcion: "",
+        img: "../img/Thor/Loki-Clasico.jpg",
+        precio: 100
+    },
+    {
+        id: 25,
+        nombre: "thor",
+        descripcion: "",
+        img: "../img/Thor/Loki.jpg",
+        precio: 100
+    },
+    {
+        id: 26,
+        nombre: "thor",
+        descripcion: "",
+        img: "../img/Thor/Mighty-thor.jpg",
+        precio: 100
+    },
+    {
+        id: 27,
+        nombre: "thor",
+        descripcion: "",
+        img: "../img/Thor/Presidente-loki.jpg",
+        precio: 100
+    },
+    {
+        id: 28,
+        nombre: "thor",
+        descripcion: "",
+        img: "../img/Thor/Sylvie.jpg",
+        precio: 100
+    },
+    {
+        id: 29,
+        nombre: "thor",
+        descripcion: "",
+        img: "../img/Thor/Thor-EndGame.jpg",
+        precio: 100
+    },
+    {
+        id: 30,
+        nombre: "thor",
+        descripcion: "",
+        img: "../img/Thor/Thor-Love-And-Thunder.jpg",
+        precio: 100
+    },
+    {
+        id: 31,
+        nombre: "vengadores",
+        descripcion: "",
+        img: "../img/Vengadores/Black-Panther-challa.jpg",
+        precio: 100
+    },
+    {
+        id: 32,
+        nombre: "vengadores",
+        descripcion: "",
+        img: "../img/Vengadores/Black-Panther.jpg",
+        precio: 100
+    },
+    {
+        id: 33,
+        nombre: "vengadores",
+        descripcion: "",
+        img: "../img/Vengadores/Black-Widow.jpg",
+        precio: 100
+    },
+    {
+        id: 34,
+        nombre: "vengadores",
+        descripcion: "",
+        img: "../img/Vengadores/Bruja-escarlata.jpg",
+        precio: 100
+    },
+    {
+        id: 35,
+        nombre: "vengadores",
+        descripcion: "",
+        img: "../img/Vengadores/Capitana-Marvel-Endgame.jpg",
+        precio: 100
+    },
+    {
+        id: 36,
+        nombre: "vengadores",
+        descripcion: "",
+        img: "../img/Vengadores/Capitana-Marvel.jpg",
+        precio: 100
+    },
+    {
+        id: 37,
+        nombre: "vengadores",
+        descripcion: "",
+        img: "../img/Vengadores/Dead-Strange.jpg",
+        precio: 100
+    },
+    {
+        id: 38,
+        nombre: "vengadores",
+        descripcion: "",
+        img: "../img/Vengadores/Doctor-Strange.jpg",
+        precio: 100
+    },
+    {
+        id: 39,
+        nombre: "vengadores",
+        descripcion: "",
+        img: "../img/Vengadores/Hulk.jpg",
+        precio: 100
+    },
+    {
+        id: 40,
+        nombre: "vengadores",
+        descripcion: "",
+        img: "../img/Vengadores/Kate-Bishop.jpg",
+        precio: 100
+    },{
+        id: 41,
+        nombre: "vengadores",
+        descripcion: "",
+        img: "../img/Vengadores/Little-groot.jpg",
+        precio: 100
+    },
+    {
+        id: 42,
+        nombre: "vengadores",
+        descripcion: "",
+        img: "../img/Vengadores/Rocket.jpg",
+        precio: 100
+    },
+    {
+        id: 43,
+        nombre: "vengadores",
+        descripcion: "",
+        img: "../img/Vengadores/Wolverine.jpg",
+        precio: 100
+    },
+    {
+        id: 44,
+        nombre: "villanos",
+        descripcion: "",
+        img: "../img/Villanos/Carnage.jpg",
+        precio: 100
+    },
+    {
+        id: 45,
+        nombre: "villanos",
+        descripcion: "",
+        img: "../img/Villanos/Doc-Ock.jpg",
+        precio: 100
+    },
+    {
+        id: 46,
+        nombre: "villanos",
+        descripcion: "",
+        img: "../img/Villanos/Duende-verde-traje.jpg",
+        precio: 100
+    },
+    {
+        id: 47,
+        nombre: "villanos",
+        descripcion: "",
+        img: "../img/Villanos/Duende-Verde.jpg",
+        precio: 100
+    },
+    {
+        id: 48,
+        nombre: "villanos",
+        descripcion: "",
+        img: "../img/Villanos/Electro.jpg",
+        precio: 100
+    },
+    {
+        id: 49,
+        nombre: "villanos",
+        descripcion: "",
+        img: "../img/Villanos/Misterio.jpg",
+        precio: 100
+    },
+    {
+        id: 50,
+        nombre: "villanos",
+        descripcion: "",
+        img: "../img/Villanos/Thano-Infinity-war.jpg",
+        precio: 100
+    },
+    {
+        id: 51,
+        nombre: "villanos",
+        descripcion: "",
+        img: "../img/Villanos/Thanos-batalla-final-endgame.jpg",
+        precio: 100
+    },
+    {
+        id: 52,
+        nombre: "villanos",
+        descripcion: "",
+        img: "../img/Villanos/Thanos.jpg",
+        precio: 100
+    },
+    {
+        id: 53,
+        nombre: "villanos",
+        descripcion: "",
+        img: "../img/Villanos/Ultron-Infinito.jpg",
+        precio: 100
+    }
+]
+
+// Variables
+
+
+const cards = document.querySelectorAll(".card");
+const btn_next = document.querySelector(".next");
+const btn_prev = document.querySelector(".prev");
+const container_cards = document.querySelector(".container-cards");
+
+const btn_section = document.querySelector(".btn-section");
+const container_section = document.querySelector(".container__section");
+const btn_div = document.querySelector(".btn-div");
+const btn_section_prev = document.querySelector(".btn-section-prev");
+
+
+
+let contador = 0;
+const lista_cards = [...cards];
+
+btn_prev.addEventListener("click", () => {
+    if(contador > 0){
+        contador--;
+        let ope = contador * -25;
+        container_cards.style.transform = `translateX(${ope}%)`;
+        container_cards.style.transition = "all 0.5s ease";
+    }else{
+        contador = 0;
+        container_cards.style.transform = `translateX(0px)`;
+    }
+     
+})
+btn_next.addEventListener("click", () => {
+    if(contador < 3){
+        contador++;
+        let ope = contador * -25;
+        container_cards.style.transform = `translateX(${ope}%)`;
+        container_cards.style.transition = "all 0.5s ease";
+    }else{
+        contador = 0;
+        container_cards.style.transform = `translateX(0px)`;
+    }
+    
+})
+
+const next_section = (e) => {
+    e.preventDefault();
+    container_section.style.transform = "translateX(-50%)";
+    container_section.style.transition = "all 1s ease";
+}
+const prev_section = (e) => {
+    e.preventDefault();
+    console.log("click");
+    container_section.style.transform = "translateX(0%)";
+    container_section.style.transition = "all 1s ease";
+}
+
+btn_section.addEventListener("click", next_section);
+btn_section_prev.addEventListener("click", prev_section);
+
+
+// function([string1, string2],target id,[color1,color2])    
+
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
+
+consoleText(['Proximo gran estreno.', '16 de febrero del 2023. Solo en cines'], 'text',['tomato','rebeccapurple']);
+
+
+//funcion hero 
+
+const hero = document.querySelector(".hero__div");
+
+
+const hero_display = () => {
+   setTimeout(() => {
+         hero.style.display = "none";
+   }, 5000);
+}
+
+hero.addEventListener("mouseover",hero_display);
+hero.addEventListener("touch",hero_display);
